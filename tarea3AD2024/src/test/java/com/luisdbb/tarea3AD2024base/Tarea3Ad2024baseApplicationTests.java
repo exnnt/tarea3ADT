774a2500e0f503bfe0170d9f4a750840d01994fb
exnnt <antoniogc59@educastur.es>
2025-03-02 12:26:57 +0100

    added dependencies for junit

--- tarea3AD2024/src/test/java/com/luisdbb/tarea3AD2024base/Tarea3Ad2024baseApplicationTests.java
+++ tarea3AD2024/src/test/java/com/luisdbb/tarea3AD2024base/Tarea3Ad2024baseApplicationTests.java
@@ -1,6 +1,7 @@
 package com.luisdbb.tarea3AD2024base;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
